6aa973f7aca8dad3ef9c2595f78a3a94ed9ddfb3
mohithreddy <mohith67821@gmail.com>
2024-12-21 19:38:04 +0530

    user can upload image to set it as profile picture

--- app.js
+++ app.js
@@ -1,28 +1,76 @@
 import express from "express";
 import bodyParser from "body-parser";
+import {dirname} from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+import multer from "multer";
 
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+
+const storage = multer.diskStorage({
+    destination : function(req,file,cb){
+        cb(null, path.join(__dirname,"public","images"));
+    },
+    filename : function(req,file,cb){
+        cb(null, Date.now()+path.extname(file.originalname));
+    },
+})
+
+const upload = multer({storage : storage});
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+let profileDetails = {};
 const blogs = [];
 
+
+app.use((req,res,next)=>{
+
+    res.locals.posts = blogs;
+    res.locals.imagesPath = imagesPath;
+    next();
+})
+
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs",{ posts : blogs});
+    res.render("index.ejs");
 })
 
 app.get("/myposts",(req,res)=>{
-    res.render("myposts.ejs",{ posts : blogs});
+    res.render("myposts.ejs");
 })
 
 
 app.get("/create",(req,res)=>{
     res.render("create.ejs");
 })
+
+app.get("/profile",(req,res)=>{
+    res.render("profile.ejs");
+})
+
+let imagesPath = __dirname + path.join("/","public","images","one.jpg");
+
+
+let avatar;
+
+
+app.post("/profile",upload.single("profile-picture"),(req,res)=>{
+
+    res.locals.profileDetails = req.body;
+    res.locals.imageDetails = req.file;
+    res.render("profile.ejs");
+})
+
+
+
 
 app.get("/home",(req,res)=>{
     res.redirect("/");
